4b238c7a3d3b7f1500cb4b74c497917baee42385
PaulCarr28 <95259406+PaulCarr28@users.noreply.github.com>
2024-01-08 18:00:24 -0700

    Fixed bullets
    The trick is to use something called "top level" in the inspector under "visibility"

--- PlayerShip.gd
+++ PlayerShip.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 @export var speed = 400
 @export var rotation_speed = PI
 
+var isAbleToShoot = true
+
+
+
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
 	
@@ -13,15 +17,23 @@ func _physics_process(delta):
 	if Input.is_action_pressed("rotate_counter_clock"):
 		rotation -= rotation_speed * delta
 	if Input.is_action_pressed("fire_weapon"):
-		const BULLET = preload("res://bullet.tscn")
-		var new_bullet = BULLET.instantiate()
-		new_bullet.global_position = %BulletSpawnPoint.global_position
-		new_bullet.global_rotation = %BulletSpawnPoint.global_rotation
-		%BulletSpawnPoint.add_child(new_bullet)
-		
+		if isAbleToShoot == true:
+			shoot()
 	if velocity.length() > 0:
 		velocity = Vector2.UP.rotated(rotation) * speed
 		
 	position += velocity * delta
+	
+	
+func shoot():
+	const BULLET = preload("res://bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %BulletSpawnPoint.global_position
+	new_bullet.global_rotation = %BulletSpawnPoint.global_rotation
+	%BulletSpawnPoint.add_child(new_bullet)
+	isAbleToShoot = false
+	%ShootCooldownTimer.start()
+	
 
-
+func _on_shoot_cooldown_timer_timeout():
+	isAbleToShoot = true
