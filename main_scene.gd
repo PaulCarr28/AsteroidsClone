f0f31a1bfbcdbffa0340e83c9d1db9567954d124
PaulCarr28 <95259406+PaulCarr28@users.noreply.github.com>
2024-01-09 15:29:56 -0700

    Worked on spawn logic for asteroids

--- main_scene.gd
+++ main_scene.gd
@@ -11,9 +11,12 @@ func _on_asteroid_timer_timeout():
 	var asteroidTypes = [preload("res://asteroid.tscn")]
 	var newAsteroid = asteroidTypes[0].instantiate()
 	
-
 	
+	%AsteroidSpawnPosition.progress_ratio = randf()
+	newAsteroid.position = %AsteroidSpawnPosition.position
 	
+	var direction = %AsteroidSpawnPosition.rotation + PI / 2
 	
+	direction += randf_range(PI/-4, PI/4)
 	
 	
